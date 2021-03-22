10842e7031fa5ea2d85ae975b8d19f76b6bc2538
Austin <wjaustinlee@gmail.com>
2021-03-22 08:52:25 -0700

    add basic page structure to app

---
+++ index.php
@@ -0,0 +1,19 @@
+<html>
+<head>
+    <title>CPSC 304 Group 45 Milestone 4</title>
+</head>
+
+<body>
+    <h1>
+        Rewards Points Administrator Terminal
+    </h1>
+    <h2>
+        Choose an option
+    </h2>
+    <h3><a href="content/account.php">Administrate Accounts</a></h3>
+    <h3><a href="content/member.php">Administrate Members</a></h3>
+    <h3><a href="content/point.php"> Points Business Analytics</a></h3>
+    <h3><a href="content/transaction.php">Transactions Business Analytics</a></h3>
+    <h3><a href="content/reward.php">Rewards Business Analytics</a></h3>
+</body>
+<?php
