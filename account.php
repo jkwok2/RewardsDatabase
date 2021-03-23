0c50cb2688c65ff9c74291665c5eca55803506dd
Austin <wjaustinlee@gmail.com>
2021-03-22 21:24:45 -0700

    change action refresh page to itself

--- account.php
+++ account.php
@@ -4,7 +4,7 @@ require 'util.php';
 require 'account-controller.php';
 ?>
 <h2>Display the Tuples in DemoTable</h2>
-<form method="GET" action="oracle-test-h2s8.php"> <!--refresh page when submitted-->
+<form method="GET" action="account.php"> <!--refresh page when submitted-->
     <input type="hidden" id="displayTupleRequest" name="displayTupleRequest">
     <input type="submit" name="displayTuples"></p>
 </form>
