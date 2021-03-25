99058b2ffcea435a36a6b74f069651524a723ef3
Austin <wjaustinlee@gmail.com>
2021-03-24 17:34:44 -0700

    add minor styling

--- account.php
+++ account.php
@@ -7,7 +7,7 @@ require 'util.php';
     <input type="hidden" id="displayTupleRequest" name="displayTupleRequest">
     <input type="submit" name="displayTuples"></p>
 </form>
-
+<hr />
 <h2>View Account Details</h2>
 <form method="get">
     <p>Enter account ID:
@@ -16,9 +16,13 @@ require 'util.php';
     <input type="submit" value="View account info" name="displayAccountDetails">
     <input type="submit" value="View account members" name="displayAccountMembers">
 </form>
+<hr />
+<!--resulting data from forms will appear here-->
+<h3>Output</h3>
 <?php
 require 'account-controller.php';
 ?>
+
     <?php
     require 'backButton.php';
     ?>
