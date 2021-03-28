caf618d69511e543a742592358c4f5aa504b9a5e
Austin <wjaustinlee@gmail.com>
2021-03-27 22:54:40 -0700

    change select * to count(*) to check table size

--- transaction-controller.php
+++ transaction-controller.php
@@ -4,7 +4,7 @@ require_once 'util.php';
 function handleDisplayTransactionRequest() {
     global $db_conn;
 
-    $cmdstr = "SELECT * FROM ORA_WJAUSTIN.TRANSACTION";
+    $cmdstr = "SELECT COUNT(*) FROM ORA_WJAUSTIN.TRANSACTION";
     $result = executePlainSQL($cmdstr);
     echo($result);
     printResult($result);
