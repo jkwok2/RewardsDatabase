8b113e6490a5d000ac03a494d6f9cd6705789d01
Austin <wjaustinlee@gmail.com>
2021-03-28 14:38:21 -0700

    add post keys echo try 2

--- transaction-controller.php
+++ transaction-controller.php
@@ -4,6 +4,8 @@ require_once 'util.php';
 function handleDisplayTransactionRequest() {
     global $db_conn;
 
+    echo array_keys ($_POST);
+    echo 'echoing post keys';
     foreach($_POST as $key=>$value)
     {
         echo "$key=$value /n";
