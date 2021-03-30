1bb25cb1880708c71f4f46848cda8681f74524bd
Austin <wjaustinlee@gmail.com>
2021-03-29 17:01:40 -0700

    add join queries to ui

--- transaction.php
+++ transaction.php
@@ -8,7 +8,7 @@ require 'util.php';
 <form method="get" action="transaction.php">
     <input type="hidden" id="displayTransactionRequest" name="displayTransactionRequest">
     <p>Use the options below to filter your result<br/>
-        <b>Instructions: values are case-sensitive! Multiple filters can be applied at once. Blank values are ignored.</b></p>
+        <b>Instructions: values are case-sensitive! Multiple filters on this form can be applied at once. Blank values are ignored.</b></p>
     <p>Filter by account id:
     <input type="text" size="40" name="accountIDFilter"></p>
     <p>Filter by merchant name:
@@ -30,8 +30,16 @@ require 'util.php';
 <h2>Advanced filter options</h2>
 <form method="get" action="transaction.php">
         <input type="hidden" id="displayAdvancedTransactionRequest" name="displayAdvancedTransactionRequest">
-    <p><b>//TODO: determine what kinds of filters to add here for join, division, etc</b></p>
-    <input type="submit" value="View filtered transactions" name="displayAdvancedTransactions">
+    <p>Use the options below to filter your result based on values from other tables<br/>
+        <b>Instructions: values are case-sensitive! Multiple filters on this form can be applied at once. Blank values are ignored.</b></p>
+    <p>Filter by account nationality:
+        <input type="text" size="40" name="accountNationFilter"></p>
+    <p>Filter by promotion rate % (enter number):
+        <input type="text" size="40" name="promotionRateFilter"></p>
+    <input type="radio" value="<=" name="less than or equal">
+    <input type="radio" value="=" name="equal">
+    <input type="radio" value=">=" name="greater than or equal">
+    <input type="submit" value="View transactions" name="displayAdvancedTransactions">
 </form>
 
 <hr />
