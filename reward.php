13ff900b4c0db6595038032c29246b786fd94d46
UBC Student <kwok95@valdes.students.cs.ubc.ca>
2021-03-30 19:00:31 -0700

    attempting to delete rewards

--- reward.php
+++ reward.php
@@ -14,21 +14,10 @@ require 'util.php';
 
 <hr />
 
-<h2>Display Rewards</h2>
+<h2>Display Available Rewards</h2>
 <form method="GET" action="reward.php"> <!--refresh page when submitted-->
     <input type="hidden" id="displayTupleRequest" name="displayTupleRequest">
     <input type="submit" name="displayTuples"></p>
-</form>
-
-<hr />
-
-<h2>Reset</h2>
-    <p>If you wish to reset the table press on the reset button. If this is the first time you're running this page, you MUST use reset</p>
-
-<form method="POST" action="reward.php">
-    <!-- if you want another page to load after the button is clicked, you have to specify that page in the action parameter -->
-    <input type="hidden" id="resetTablesRequest" name="resetTablesRequest">
-    <p><input type="submit" value="Reset" name="reset"></p>
 </form>
 
 <!--resulting output from forms will appear here-->
