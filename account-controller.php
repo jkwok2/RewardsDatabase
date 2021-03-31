1a4b041d30cdad126da914c2894274ff80e1cfb3
UBC Student <kwok95@valdes.students.cs.ubc.ca>
2021-03-30 20:05:25 -0700

    cleaned up add account

--- account-controller.php
+++ account-controller.php
@@ -38,9 +38,7 @@ function handleInsertRequest() {
 // A better coding practice is to have one method that reroutes your requests accordingly. It will make it easier to add/remove functionality.
 function handlePOSTRequest() {
     if (connectToDB()) {
-        if (array_key_exists('deleteAccountRequest', $_POST)) {
-            handleDeleteAccountRequest();
-        } else if (array_key_exists('insertQueryRequest', $_POST)) {
+        if (array_key_exists('insertQueryRequest', $_POST)) {
             handleInsertRequest();
         }
 
@@ -52,12 +50,8 @@ function handlePOSTRequest() {
 // A better coding practice is to have one method that reroutes your requests accordingly. It will make it easier to add/remove functionality.
 function handleGETRequest() {
     if (connectToDB()) {
-        if (array_key_exists('displayAccountDetails', $_GET)) {
-            handleDisplayAccountDetailsRequest();
-        } else if (array_key_exists('displayTuples', $_GET)) {
+        if (array_key_exists('displayTuples', $_GET)) {
             handleDisplayRequest();
-        } else if (array_key_exists('displayAccountMembers', $_GET)) {
-            handleDisplayAccountMembersRequest();
         }
 
         disconnectFromDB();
